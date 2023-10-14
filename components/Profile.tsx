fe2180c1eabf55abfdb503f7295376960636811c
Taylor Svec <taylorsvec978@yahoo.com>
2023-10-13 21:58:21 -0400

    test

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -42,7 +42,7 @@ export default function Profile() {
               </DropdownMenuItem>
             </Link>
 
-            <SignOutButton signOutCallback={() => router.push("/")}>
+            <SignOutButton>
               <DropdownMenuItem className="cursor-pointer">
                 Sign Out
               </DropdownMenuItem>
